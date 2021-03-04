25a88ca7b81656da15f011d8ae7d3d50e39a7028
Aaron Zwiebel <aaronz@steps.me>
2021-03-04 17:12:19 +0200

    Initial commit

---
+++ Demo.py
@@ -0,0 +1,57 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+
+#Calls the chrome driver
+driver = webdriver.Chrome(executable_path="Resources/chromedriver.exe")
+# Takes you to the website
+driver.get("http://saucedemo.com/")
+
+#Finds the username and password by there name in the HTML element
+username=driver.find_element_by_name("user-name")
+password=driver.find_element_by_name("password")
+
+#Sends the keystrokes that make the username and password to said elements
+username.send_keys("standard_user")
+password.send_keys("secret_sauce")
+
+#Presses the Login button
+login=driver.find_element_by_id("login-button").click()
+
+
+#Waits until the element "product_sort_container" is visible inside the html code to start the next portion
+element = WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "product_sort_container")))
+
+#Finds the dropdown menu by its xpath
+dropDown = Select(driver.find_element_by_xpath('/html/body/div[1]/div/div/div[2]/div/div[1]/div[2]/select'))
+#select the "lohi" value of said dropdown menu
+dropDown.select_by_value('lohi')
+#Clicks the baby shirt, then purchase, then back one, all through the xpath, waiting isnt needed because they are all done at once
+(driver.find_element_by_xpath('/html/body/div[1]/div/div/div[2]/div/div[2]/div/div[1]/div[2]/a/div')).click()
+(driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[2]/div/div/div/button')).click()
+(driver.find_element_by_xpath('/html/body/div[1]/div/div[2]/div[2]/div/button')).click()
+
+#Finds shopping cart and continues via id and xpath respectively
+driver.find_element_by_id("shopping_cart_container").click()
+driver.find_element_by_xpath("/html/body/div[1]/div/div/div[3]/div/div[2]/a[2]").click()
+
+#Finds the first name, last name, and postal code boxes via the element names
+first_name = driver.find_element_by_id("first-name")
+last_name = driver.find_element_by_id("last-name")
+postal_code = driver.find_element_by_id("postal-code")
+
+#Sends the name keys
+first_name.send_keys("Test")
+last_name.send_keys("Name")
+postal_code.send_keys("330l7h")
+
+#Finish
+driver.find_element_by_xpath("/html/body/div[1]/div/div/div[3]/div/form/div[2]/input").click()
+driver.find_element_by_xpath("/html/body/div[1]/div/div/div[3]/div/div[2]/div[8]/a[2]").click()
+
+#closes the driver
+driver.close()
